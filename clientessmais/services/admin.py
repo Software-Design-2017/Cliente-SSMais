e11c7228418bc3d710fb6822d326d71045649e93
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-11-27 01:04:30 -0200

    Adding version 1.0.

--- clientessmais/services/admin.py
+++ clientessmais/services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    ServiceHair, ServiceBeard
+)
+
+admin.site.register(ServiceHair)
+admin.site.register(ServiceBeard)
